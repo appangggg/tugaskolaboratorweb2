50d62b5af21d2094c2009313a54e2cb1fe51b2e3
appangggg <60200124013@uin.alauddin-ac.id>
2026-05-04 19:25:45 +0800

    feat: setup project laravel dan database

---
+++ resources/views/tasks.blade.php
@@ -0,0 +1,11 @@
+
+<!DOCTYPE html>
+<html>
+<head><title>To-Do List</title></head>
+<body>
+    <h2>Aplikasi To-Do List Kolaborasi</h2>
+    <!-- TEMPAT FORM CREATE -->
+    <hr>
+    <!-- TEMPAT TABLE READ & DELETE -->
+</body>
+</html>
