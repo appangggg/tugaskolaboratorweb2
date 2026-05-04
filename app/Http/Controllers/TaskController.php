6651d91700e90d154938c1d0871827f10d2166f1
appangggg <60200124013@uin.alauddin-ac.id>
2026-05-04 21:50:35 +0800

    fix bug

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,27 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
-use App\Models\Task;
 
 class TaskController extends Controller
 {
- feature/read-delete-task
+    // Fitur Read (Milik Temanmu)
     public function index() {
-         $tasks = Task::latest()->get();
-         return view('tasks', compact('tasks'));
-     }
+        $tasks = Task::latest()->get();
+        return view('tasks', compact('tasks'));
+    }
 
-     public function destroy(Task $task) {
-         $task->delete();
-         return redirect()->back();
-     }
-}
-
-    // Fungsi Create (POST) harus berada di dalam class ini
+    // Fitur Create (Milikmu)
     public function store(Request $request) {
         $request->validate(['task_name' => 'required']);
         Task::create(['task_name' => $request->task_name]);
         return redirect()->back();
     }
+
+    // Fitur Delete (Milik Temanmu)
+    public function destroy(Task $task) {
+        $task->delete();
+        return redirect()->back();
+    }
 }
-main
